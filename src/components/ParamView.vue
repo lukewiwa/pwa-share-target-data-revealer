fce032b43cbe3f78946f5129bcf5efb540f607de
lukewiwa <luke.wiwa@gmail.com>
2020-01-17 14:40:30 +1100

    Space out params

--- src/components/ParamView.vue
+++ src/components/ParamView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-row flex-no-wrap bg-teal-200 p-3 overflow-x-auto min-w-full max-w-xl rounded text-gray-700"
+    class="flex flex-row flex-no-wrap bg-teal-200 p-3 m-2 overflow-x-auto min-w-full max-w-xl rounded text-gray-700"
   >
     <h1 class="font-bold mr-2">{{ paramKey }}:</h1>
     <span class="inline">{{ paramValue }}</span>
