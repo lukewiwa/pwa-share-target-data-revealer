90b5fa922b12bdb4e4eb76a639554cbaecb679ca
lukewiwa <luke.wiwa@gmail.com>
2020-01-17 16:13:16 +1100

    Improve PWA colours

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@ module.exports = {
   publicPath: "/pwa-share-target-data-revealer/",
   pwa: {
     name: "PWA Share Target Data Revealer",
-    themeColor: "#4DBA87", // The Vue color
+    themeColor: "#4FD1C5",
     manifestPath: "manifest.webmanifest",
     manifestOptions: {
       short_name: "Share Target Revealer",
+      background_color: "#E6FFFA",
       start_url: ".",
       display: "standalone",
       share_target: {
